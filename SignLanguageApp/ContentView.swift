0562b88af25e2768dc6f5738583ec2302a079513
rahdeva <wirandanaputra@gmail.com>
2026-07-16 20:00:55 +0700

    Initial Commit

---
+++ SignLanguageApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SignLanguageApp
+//
+//  Created by rahdeva on 16/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
